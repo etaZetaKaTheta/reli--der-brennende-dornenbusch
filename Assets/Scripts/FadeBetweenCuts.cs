bd14085c16c668097092ed772cd680059873b207
Ratchgamer123 <trautwein.maximilian04@gmail.com>
2022-03-10 00:39:31 +0100

    first cutscene

---
+++ Assets/Scripts/FadeBetweenCuts.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeBetweenCuts : MonoBehaviour
+{
+    [SerializeField] private float fadeOutTime = 1.0f;
+    [SerializeField] private float transitionTime = 0.2f;
+    [SerializeField] private GameObject fadeOutObject;
+    [SerializeField] private GameObject timeline;
+
+    void OnEnable()
+    {
+        StartCoroutine(FadeCutscene());
+    }
+
+    private IEnumerator FadeCutscene()
+    {
+        Image image = fadeOutObject.GetComponent<Image>();
+        LeanTween.value(gameObject, 0, 1, fadeOutTime).setOnUpdate((float val) =>
+        {
+            Color c = image.color;
+            c.a = val;
+            image.color = c;
+        });
+
+        yield return new WaitForSeconds(transitionTime);
+
+        timeline.SetActive(false);
+
+        image.color = Color.white;
+        LeanTween.value(gameObject, 1, 0, fadeOutTime).setOnUpdate((float val) =>
+        {
+            Color c = image.color;
+            c.a = val;
+            image.color = c;
+        });
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform orientation;
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float movementMultiplier = 10f;
+    [SerializeField] private float airMultiplier = 0.4f;
+    [SerializeField] private float gravity = 9.81f;
+
+    [Header("Camera")]
+    public float sensX = 50.0f;
+    public float sensY = 50.0f;
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform head;
+    [SerializeField] private Camera camStats;
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    public Quaternion TargetRotation { private set; get; }
+
+    [Header("Sprinting")]
+    [SerializeField] float walkSpeed = 4f;
+    [SerializeField] float sprintSpeed = 6f;
+    [SerializeField] float acceleration = 10f;
+    [SerializeField] float sprintingFOV = 90f;
+    [SerializeField] float normalFOV = 70f;
+    [SerializeField] float fovChangeSpeed = 1f;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce = 5f;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Drag")]
+    [SerializeField] private float groundDrag = 6f;
+    [SerializeField] private float airDrag = 2f;
+
+    float horizontalMovement;
+    float verticalMovement;
+
+    [Header("Ground Detection")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+    float groundDistance = 0.4f;
+
+    bool isGrounded;
+
+    Vector3 moveDirection;
+    Vector3 slopeMoveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        TargetRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        cam.position = head.position;
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        MyInput();
+
+        ControlDrag();
+        ControlSpeed();
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+
+    }
+
+    void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
+
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * multiplier;
+        xRotation -= mouseY * sensY * multiplier;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
+        orientation.transform.localRotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    private void Jump()
+    {
+        //rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
+    }
+
+    private void ControlDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        rb.AddForce(Physics.gravity * gravity, ForceMode.Acceleration);
+        if (isGrounded && !OnSlope())
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
+    }
+
+    private void ControlSpeed()
+    {
+        if (Input.GetKey(sprintKey) && isGrounded)
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
+            camStats.fieldOfView = Mathf.Lerp(camStats.fieldOfView, sprintingFOV, fovChangeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+            Mathf.Lerp(camStats.fieldOfView, normalFOV, fovChangeSpeed * Time.deltaTime);
+        }
+    }
+
+    RaycastHit slopeHit;
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, 1.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void ResetTargetRotation()
+    {
+        TargetRotation = Quaternion.LookRotation(transform.forward, Vector3.up);
+    }
+}
